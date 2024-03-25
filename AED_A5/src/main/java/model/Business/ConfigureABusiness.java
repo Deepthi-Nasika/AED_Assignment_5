2092e2f3c5ad3b3014858ada092174102db247d0
Deepthi-Nasika <nasika.d@northeastern.edu>
2024-03-25 16:10:27 -0400

    Manage Products

--- AED_A5/src/main/java/model/Business/ConfigureABusiness.java
+++ AED_A5/src/main/java/model/Business/ConfigureABusiness.java
@@ -29,99 +29,129 @@ import model.UserAccountManagement.UserAccountDirectory;
 
 /**
  *
- * @author kal bugrara
+ * @author nasik
  */
 public class ConfigureABusiness {
 
   public static Business initialize() {
-    Business business = new Business("Xerox");
+    Business business = new Business("Stationary");
     
+    // Create Directory
     SupplierDirectory suplierdirectory = business.getSupplierDirectory();
- 
-        Supplier supplier1 = suplierdirectory.newSupplier("Lenovo");
-        ProductCatalog productcatalog = supplier1.getProductCatalog();
-        Product products1p1 = productcatalog.newProduct("Scanner 3  1", 2000, 16500, 10000);
-        Product products1p2 = productcatalog.newProduct("Scanner 4", 10000, 25000, 16500);
-        Product products1p3 = productcatalog.newProduct("Printer 2", 22000, 40000, 36500);
-        Product products1p4 = productcatalog.newProduct("Photocopier 2 ", 30000, 70000, 50000);
-        Product products1p5 = productcatalog.newProduct("Scanner  5", 19000, 36500, 25000);
-        Product products1p6 = productcatalog.newProduct("Scanner 6", 90000, 125000, 105000);
-        Product products1p7 = productcatalog.newProduct("Printer 3", 22000, 60000, 36500);
-        Product products1p8 = productcatalog.newProduct("Photocopier 3", 30000, 70000, 50000);
- 
-        Supplier supplier2 = suplierdirectory.newSupplier("Epson");
-        productcatalog = supplier2.getProductCatalog();
-        Product products2p1 = productcatalog.newProduct("Scanner 13  1", 12000, 26000, 18500);
-        Product products2p2 = productcatalog.newProduct("Scanner 14", 90000, 165000, 125000);
-        Product products2p3 = productcatalog.newProduct("Color Printer 112", 422000, 540000, 495000);
-        Product products2p4 = productcatalog.newProduct("Photocopier 922 ", 430000, 890000, 550000);
-        Product products2p5 = productcatalog.newProduct("Low toner Scanner  102", 195000, 500100, 365102);
-        Product products2p6 = productcatalog.newProduct("Speedy color Scanner 611", 900000, 125000, 1650000);
-        Product products2p7 = productcatalog.newProduct("Premier Printer 300", 322000, 470000, 736500);
-        Product products2p8 = productcatalog.newProduct("Color Photocopier 500", 350000, 580000, 780000);
- 
-// Create Persons
-        PersonDirectory persondirectory = business.getPersonDirectory();
-// person representing sales organization        
-        Person xeroxsalesperson001 = persondirectory.newPerson("Xerox sales");
-        Person xeroxmarketingperson001 = persondirectory.newPerson("Xerox marketing");
-        Person xeroxadminperson001 = persondirectory.newPerson("Xerox admin");
- 
- 
-// Create person objects to represent customer organizations. 
-        Person person005 = persondirectory.newPerson("Dell");
-        Person person006 = persondirectory.newPerson("Microsoft");
-        Person person007 = persondirectory.newPerson("Google");
-        Person person008 = persondirectory.newPerson("JP Morgan");
-        Person person009 = persondirectory.newPerson("State street"); //we use this as customer
- 
-// Create Customers
-        CustomerDirectory customedirectory = business.getCustomerDirectory();
-        CustomerProfile customerprofile1 = customedirectory.newCustomerProfile(person005);
-        CustomerProfile customerprofile2 = customedirectory.newCustomerProfile(person006);
-        CustomerProfile customerprofile3 = customedirectory.newCustomerProfile(person007);
-        CustomerProfile customerprofile4 = customedirectory.newCustomerProfile(person008);
-        CustomerProfile customerprofile5 = customedirectory.newCustomerProfile(person009);
- 
-// Create Sales people
-        SalesPersonDirectory salespersondirectory = business.getSalesPersonDirectory();
-        SalesPersonProfile salespersonprofile = salespersondirectory.newSalesPersonProfile(xeroxsalesperson001);
- 
-// Create Marketing people
-        MarketingPersonDirectory marketingpersondirectory = business.getMarketingPersonDirectory();
-        MarketingPersonProfile marketingpersonprofile0 = marketingpersondirectory.newMarketingPersonProfile(xeroxmarketingperson001);
- 
-// Create Admins to manage the business
-//       EmployeeDirectory employeedirectory = business.getEmployeeDirectory();
-//       EmployeeProfile employeeprofile0 = employeedirectory.newEmployeeProfile(xeroxadminperson001);
- 
-// Create User accounts that link to specific profiles
-        UserAccountDirectory uadirectory = business.getUserAccountDirectory();
-        UserAccount ua1 = uadirectory.newUserAccount(salespersonprofile, "Sales", "XXXX"); /// order products for one of the customers and performed by a sales person
-        UserAccount ua2 = uadirectory.newUserAccount(marketingpersonprofile0, "Marketing", "XXXX"); /// order products for one of the customers and performed by a sales person
-//       UserAccount ua3 = uadirectory.newUserAccount(employeeprofile0, "Admin", "XXXX"); /// order products for one of the customers and performed by a sales person
- 
-// Process Orders on behalf of sales person and customer
-        MasterOrderList masterorderlist = business.getMasterOrderList();
-        Order order1 = masterorderlist.newOrder(customerprofile4, salespersonprofile);
-        OrderItem oi1 = order1.newOrderItem(products1p1, 18000, 1);
-        OrderItem oi2 = order1.newOrderItem(products1p2, 19500, 4);
-        OrderItem oi3 = order1.newOrderItem(products1p3, 36500, 10);
-        OrderItem oi4 = order1.newOrderItem(products1p4, 50000, 1);
-        OrderItem oi5 = order1.newOrderItem(products1p5, 25000, 3);
-        OrderItem oi6 = order1.newOrderItem(products1p6, 105000, 2);
-        OrderItem oi7 = order1.newOrderItem(products1p7, 36500, 3);
-        OrderItem oi8 = order1.newOrderItem(products1p8, 50000, 2);
- 
-        Order order12 = masterorderlist.newOrder(customerprofile4, salespersonprofile);
-        OrderItem oi112 = order1.newOrderItem(products1p1, 17000, 1);
-        OrderItem oi12 = order1.newOrderItem(products1p2, 9500, 4);
-        OrderItem oi13 = order1.newOrderItem(products1p3, 29500, 10);
-        OrderItem oi14 = order1.newOrderItem(products1p4, 30000, 1);
-        OrderItem oi15 = order1.newOrderItem(products1p5, 2000, 3);
-        OrderItem oi16 = order1.newOrderItem(products1p6, 95000, 2);
-        OrderItem oi17 = order1.newOrderItem(products1p7, 26500, 3);
-        OrderItem oi18 = order1.newOrderItem(products1p8, 40000, 2);
+    PersonDirectory persondirectory = business.getPersonDirectory();
+    SalesPersonDirectory salesPersonDirectory = business.getSalesPersonDirectory();
+    CustomerDirectory customerDirectory = business.getCustomerDirectory();
+    UserAccountDirectory userAccountDirectory = business.getUserAccountDirectory();
+    MasterOrderList masterorderlist = business.getMasterOrderList();
+    
+    // Create Suppliers
+    Supplier supplier1 = suplierdirectory.newSupplier("Apsara");
+    
+    // Create Products
+    ProductCatalog productcatalog = supplier1.getProductCatalog();
+    Product p1 = productcatalog.newProduct("Pen", 1000, 5000, 2000);
+    Product p2 = productcatalog.newProduct("Pencil", 500, 3000, 1500);
+    Product p3 = productcatalog.newProduct("Notebook", 2000, 8000, 4000);
+    Product p4 = productcatalog.newProduct("Eraser", 300, 1500, 750);
+    Product p5 = productcatalog.newProduct("Ruler", 400, 2000, 1000);
+    Product p6 = productcatalog.newProduct("Highlighter", 800, 4000, 2000);
+    Product p7 = productcatalog.newProduct("Marker", 1200, 6000, 3000);
+    Product p8 = productcatalog.newProduct("Stapler", 1500, 7000, 3500);
+    Product p9 = productcatalog.newProduct("Scissors", 1000, 5000, 2500);
+    Product p10 = productcatalog.newProduct("Glue Stick", 600, 3000, 1500);
+    
+    
+    
+    // Create Sales_Person
+    Person salesperson1 = persondirectory.newPerson("SalesPerson1");
+    Person salesperson2 = persondirectory.newPerson("SalesPerson2");
+    
+    SalesPersonProfile sp1 = salesPersonDirectory.newSalesPersonProfile(salesperson1); 
+    SalesPersonProfile sp2 = salesPersonDirectory.newSalesPersonProfile(salesperson2);
+    
+    // Create User Accounts
+    UserAccount userAccount1 = userAccountDirectory.newUserAccount(sp1, "user1", "pass1");
+    UserAccount userAccount2 = userAccountDirectory.newUserAccount(sp2, "user2", "pass2");
+    
+    // Create Customers
+    Person customer1 = persondirectory.newPerson("Customer1");
+    Person customer2 = persondirectory.newPerson("Customer2");
+    Person customer3 = persondirectory.newPerson("Customer3");
+    Person customer4 = persondirectory.newPerson("Customer4");
+    Person customer5 = persondirectory.newPerson("Customer5");
+    
+    CustomerProfile cp1 = customerDirectory.newCustomerProfile(customer1);
+    CustomerProfile cp2 = customerDirectory.newCustomerProfile(customer2);
+    CustomerProfile cp3 = customerDirectory.newCustomerProfile(customer3);
+    CustomerProfile cp4 = customerDirectory.newCustomerProfile(customer4);
+    CustomerProfile cp5 = customerDirectory.newCustomerProfile(customer5);
+    
+    // Process Orders on behalf of sales person and customer
+    // Order-1 (Customer 1, SalesPerson 1)
+    Order order1 = masterorderlist.newOrder(cp1, sp1);
+    order1.newOrderItem(p1, 2500, 20);
+    order1.newOrderItem(p2, 1500, 15);
+    order1.newOrderItem(p3, 1000, 10);
+    order1.newOrderItem(p4, 500, 5);
+    order1.newOrderItem(p5, 200, 2);
+    order1.newOrderItem(p6, 400, 4);
+    order1.newOrderItem(p7, 600, 6);
+    order1.newOrderItem(p8, 700, 7);
+    order1.newOrderItem(p9, 500, 5);
+    order1.newOrderItem(p10, 300, 3);
+
+    // Order 2 (Customer 2, SalesPerson 1)
+    Order order2 = masterorderlist.newOrder(cp2, sp1);
+    order2.newOrderItem(p1, 3000, 20);
+    order2.newOrderItem(p2, 2000, 15);
+    order2.newOrderItem(p3, 1500, 20);
+    order2.newOrderItem(p4, 800, 10);
+    order2.newOrderItem(p5, 500, 4);
+    order2.newOrderItem(p6, 700, 6);
+    order2.newOrderItem(p7, 800, 8);
+    order2.newOrderItem(p8, 900, 3);
+    order2.newOrderItem(p9, 800, 5);
+    order2.newOrderItem(p10, 500, 2);
+
+    // Order 3 (Customer 3, SalesPerson 1)
+    Order order3 = masterorderlist.newOrder(cp3, sp1);
+    order3.newOrderItem(p1, 1800, 10);
+    order3.newOrderItem(p2, 500, 5);
+    order3.newOrderItem(p3, 500, 0);
+    order3.newOrderItem(p4, 700, 2);
+    order3.newOrderItem(p5, 900, 2);
+    order3.newOrderItem(p6, 900, 8);
+    order3.newOrderItem(p7, 300, 6);
+    order3.newOrderItem(p8, 300, 3);
+    order3.newOrderItem(p9, 700, 2);
+    order3.newOrderItem(p10, 400, 8);
+
+    // Order 4 (Customer 4, SalesPerson 1)
+    Order order4 = masterorderlist.newOrder(cp4, sp1);
+    order4.newOrderItem(p1, 800, 20);
+    order4.newOrderItem(p2, 1300, 5);
+    order4.newOrderItem(p3, 300, 4);
+    order4.newOrderItem(p4, 800, 9);
+    order4.newOrderItem(p5, 100, 3);
+    order4.newOrderItem(p6, 200, 4);
+    order4.newOrderItem(p7, 400, 3);
+    order4.newOrderItem(p8, 300, 2);
+    order4.newOrderItem(p9, 200, 8);
+    order4.newOrderItem(p10, 250, 5);
+
+    // Order 5 (Customer 5, SalesPerson 1)
+    Order order5 = masterorderlist.newOrder(cp5, sp1);
+    order5.newOrderItem(p1, 500, 5);
+    order5.newOrderItem(p2, 900, 12);
+    order5.newOrderItem(p3, 800, 1);
+    order5.newOrderItem(p4, 300, 9);
+    order5.newOrderItem(p5, 500, 8);
+    order5.newOrderItem(p6, 700, 8);
+    order5.newOrderItem(p7, 800, 16);
+    order5.newOrderItem(p8, 200, 7);
+    order5.newOrderItem(p9, 600, 15);
+    order5.newOrderItem(p10, 800, 23);
+
     
     return business;
   }
